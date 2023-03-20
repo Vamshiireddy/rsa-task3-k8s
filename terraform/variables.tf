b249ddefb7cb6ee6a3518cf3d1a70b5ed01e7de2
Vamshiireddy <128018988+Vamshiireddy@users.noreply.github.com>
2023-03-21 02:09:07 +0530

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+
+variable "appId" {
+  description = "Azure Kubernetes Service Cluster service principal"
+  default = "XXXX"
+}
+
+variable "password" {
+  description = "Azure Kubernetes Service Cluster password"
+  default = "XXXX"
+}
